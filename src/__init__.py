034653d97655394f8de3752619b9e60e7dbd5219
Joaquin Diaz <diaz.joaquin.e@gmail.com>
2025-07-21 15:36:29 -0300

    Remove create_unit from __all__ in __init__.py to streamline module exports

--- src/__init__.py
+++ src/__init__.py
@@ -1,11 +1,11 @@
-from .units import Unit, Pikeman, Archer, Knight, create_unit
+from .units import Unit, Pikeman, Archer, Knight
 from .civilizations import Civilization, CivilizationConfig
 from .army import Army, InsufficientGoldError, InsufficientUnitsError, InvalidTransformationError
 from .battle import BattleSystem, BattleRecord, BattleResult
 
 __all__ = [
     # Units
-    'Unit', 'Pikeman', 'Archer', 'Knight', 'create_unit',
+    'Unit', 'Pikeman', 'Archer', 'Knight',
     # Civilizations
     'Civilization', 'CivilizationConfig',
     # Army
